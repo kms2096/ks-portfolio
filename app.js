14962d5f3a419e41dcd81852c48b04927d6730a5
kms2096 <kms2096@g.rit.edu>
2023-08-30 22:29:21 -0400

    Added tag filters

--- app.js
+++ app.js
@@ -5,3 +5,57 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 })
+
+
+filterSelection("all")
+
+function filterSelection(c) {
+var x, i;
+x = document.getElementsByClassName("filterDiv");
+if (c == "all") c = "";
+// Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+for (i = 0; i < x.length; i++) {
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+}
+}
+
+// Show filtered elements
+function AddClass(element, name) {
+var i, arr1, arr2;
+arr1 = element.className.split(" ");
+arr2 = name.split(" ");
+for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+    element.className += " " + arr2[i];
+    }
+}
+}
+
+// Hide elements that are not selected
+function RemoveClass(element, name) {
+var i, arr1, arr2;
+arr1 = element.className.split(" ");
+arr2 = name.split(" ");
+for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+    arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+}
+element.className = arr1.join(" ");
+}
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("btn_container");
+var btns = btnContainer.getElementsByClassName("btn");
+
+for (var i = 0; i < btns.length; i++) {
+btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    // TODO: These lines don't seem to run, current and this don't seem to work properly
+    65
+    // current[0].className = current[0].className.replace(" active", "");
+    // this.className += " active";
+});
+}
+    
