ce5765f722a9868c2855000b3ba9132f338a26b2
kms2096 <kms2096@g.rit.edu>
2023-09-01 16:16:43 -0400

    Fixed placement of btn, added btn functionality

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ menu.addEventListener('click', function() {
 })
 
 
-filterSelection("all")
+//filterSelection("all")
 
-function filterSelection(c) {
-var x, i;
-x = document.getElementsByClassName("filterDiv");
-if (c == "all") c = "";
-// Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
-}
-}
+// function filterSelection(c) {
+// var x, i;
+// x = document.getElementsByClassName("filterDiv");
+// if (c == "all") c = "";
+// // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+// for (i = 0; i < x.length; i++) {
+//     RemoveClass(x[i], "show");
+//     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+// }
+// }
 
 // Show filtered elements
 function AddClass(element, name) {
@@ -46,16 +46,15 @@ element.className = arr1.join(" ");
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("btn_container");
-var btns = btnContainer.getElementsByClassName("btn");
+// var btnContainer = document.getElementById("btn_container");
+// var btns = btnContainer.getElementsByClassName("btn");
 
-for (var i = 0; i < btns.length; i++) {
-btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    // TODO: These lines don't seem to run, current and this don't seem to work properly
-    65
-    // current[0].className = current[0].className.replace(" active", "");
-    // this.className += " active";
-});
-}
+// for (var i = 0; i < btns.length; i++) {
+// btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     // TODO: These lines don't seem to run, current and this doesn't seem to work properly
+//     // current[0].className = current[0].className.replace(" active", "");
+//     // this.className += " active";
+// });
+// }
     
